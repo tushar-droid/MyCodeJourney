1960a0990130095852406429a2c672908ff15ff4
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-12-10 22:27:36 -0800

    eslint setup done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("Ayyo this is hooked up now");
+console.log('Ayyo this is hooked up now');
+console.log('dfsdf');
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const path = require("path");
+const path = require('path');
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(jpeg|jpg|png|svg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|ttf|eot|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
