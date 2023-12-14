f80a8a49379e6d26e596c67c647a4c2a1c1dd7ef
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-12-14 02:01:25 -0800

    Changed the path to image to be added from JS not HTMl

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import profileImage from './assets/profile.jpg';
+import footerImage from './assets/footer.jpg';
+
+const profileImgContainer = document.querySelector('.profile');
+const footerImgContainer = document.querySelector('.footer-img');
+profileImgContainer.src = profileImage;
+footerImgContainer.src = footerImage;
