19121a6239307d1bc3734d1f3ec216755807fd56
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-12-28 00:50:28 -0800

    Added battleship game to the website

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './styles.css';
 import profileImage from './assets/profile.jpg';
 import footerImage from './assets/footer.jpg';
+import battleShipImage from './assets/battleship.png';
 
 const profileImgContainer = document.querySelector('.profile');
 const footerImgContainer = document.querySelector('.footer-img');
+const battleImage = document.querySelector('.battleship-image-container');
+battleImage.src = battleShipImage;
 profileImgContainer.src = profileImage;
 footerImgContainer.src = footerImage;
