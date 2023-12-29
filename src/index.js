39a6e764d955affa6f3a3886d5c86baacafecbcb
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-12-29 02:12:24 -0800

    added projects to the website

--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ import './styles.css';
 import profileImage from './assets/profile.jpg';
 import footerImage from './assets/footer.jpg';
 import battleShipImage from './assets/battleship.png';
+import weatherImage from './assets/weather-forecast.png';
+import listImage from './assets/list.png';
+import dashImage from './assets/landing-page.png';
+import topImage from './assets/top.png';
+import fccImage from './assets/fcc.png';
 
 const profileImgContainer = document.querySelector('.profile');
 const footerImgContainer = document.querySelector('.footer-img');
-const battleImage = document.querySelector('.battleship-image-container');
-battleImage.src = battleShipImage;
+const battleImageContainer = document.querySelector('.battleship-image-container');
+const weatherImageContainer = document.querySelector('.weather-image-container');
+const todoListImageContainer = document.querySelector('.todoList-image-container');
+const dashImageContainer = document.querySelector('.dash-image-container');
+const topImageContainer = document.querySelector('.top-image-container');
+const fccImageContainer = document.querySelector('.fcc-image-container');
+
+battleImageContainer.src = battleShipImage;
 profileImgContainer.src = profileImage;
 footerImgContainer.src = footerImage;
+weatherImageContainer.src = weatherImage;
+todoListImageContainer.src = listImage;
+dashImageContainer.src = dashImage;
+topImageContainer.src = topImage;
+fccImageContainer.src = fccImage;
+
+const cards = document.querySelectorAll('.project-card');
+
+cards.forEach((card) => {
+  card.addEventListener('click', (e) => {
+    if (e.target.classList.contains('repo-link')) return;
+    const link = card.getElementsByClassName('link')[0].href;
+    window.open(link, '_blank');
+  });
+});
